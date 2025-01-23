b059f94f1fb8b8515ebeccd6726a61795f69a5f7
andreaDevelope <andrea_develope@outlook.it>
2025-01-23 21:23:17 +0100

    fixed http.get homeService

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,23 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { HomeService } from "../../services/home.service";
+import { timeInterval } from "rxjs";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  cliente!: any;
+  constructor(private homeServ: HomeService) {}
+
+  ngOnInit() {
+    this.getById();
+    setTimeout(() => console.log(this.cliente), 4000);
+  }
+
+  getById() {
+    this.homeServ.getById(1).subscribe((response) => (this.cliente = response));
+  }
+}
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,9 +1,22 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class HomeService {
+  private baseUrl = "http://localhost:8080/api/cliente"; // Endpoint del backend
+  private token =
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTczNzY2MzU3OCwiZXhwIjoxNzM3NjY3MTc4fQ.L1gUAc8GD-RUm-2YGv4CTqGwqQJUILD4vxFMfOaGq-s"; // Inserisci qui il token hardcoded
+
   constructor(private http: HttpClient) {}
+
+  getById(id: number): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`, // Aggiunge il token hardcoded agli header
+    });
+
+    return this.http.get<any>(`${this.baseUrl}/${id}`, { headers });
+  }
 }
