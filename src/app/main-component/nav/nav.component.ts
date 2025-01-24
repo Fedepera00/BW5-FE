ba487689d3fc5ebccced74050923d93853c27b14
andreaDevelope <andrea_develope@outlook.it>
2025-01-24 15:42:12 +0100

    fixed navbar

--- src/app/main-component/nav/nav.component.ts
+++ src/app/main-component/nav/nav.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: '.app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss'],
+  selector: ".app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.scss"],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  selectedIndex = 0; // Valore predefinito (prima scheda)
+  routes = ["/", "/login", "/home", "/create-cliente"]; // Percorsi associati alle schede
+
   constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    // Ascolta i cambiamenti di navigazione per aggiornare l'indice della scheda
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const index = this.routes.indexOf(event.urlAfterRedirects);
+        if (index !== -1) {
+          this.selectedIndex = index; // Aggiorna la scheda attiva
+        }
+      }
+    });
+  }
+
   navigate(index: number): void {
-    const routes = ['/', '/login', '/home'];
-    this.router.navigate([routes[index]]);
+    this.router.navigate([this.routes[index]]);
   }
 }
