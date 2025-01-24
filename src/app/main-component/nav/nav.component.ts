15205e9049cb3f6e67571c40f4272ab00f7e5001
andreaDevelope <andrea_develope@outlook.it>
2025-01-24 15:51:05 +0100

    fixed navbar

--- src/app/main-component/nav/nav.component.ts
+++ src/app/main-component/nav/nav.component.ts
@@ -7,18 +7,17 @@ import { Router, NavigationEnd } from "@angular/router";
   styleUrls: ["./nav.component.scss"],
 })
 export class NavComponent implements OnInit {
-  selectedIndex = 0; // Valore predefinito (prima scheda)
-  routes = ["/", "/login", "/home", "/create-cliente"]; // Percorsi associati alle schede
+  selectedIndex = 0;
+  routes = ["/", "/login", "/home", "/create-cliente"];
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // Ascolta i cambiamenti di navigazione per aggiornare l'indice della scheda
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         const index = this.routes.indexOf(event.urlAfterRedirects);
         if (index !== -1) {
-          this.selectedIndex = index; // Aggiorna la scheda attiva
+          this.selectedIndex = index;
         }
       }
     });
